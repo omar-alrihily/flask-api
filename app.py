16b21018b211ea7f744bfb1036d0cfc8a870975d
omar-alrihily <omar.8260@gmail.com>
2023-12-28 16:13:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import pandas as pd
 import pickle
 import numpy as np
 from flask import Flask, request, jsonify
-from flask_cors import CORS
+
 
 app = Flask(__name__)
-CORS(app)
+
 
 # Load your LG_model2
 with open('LG_model2.pkl', 'rb') as model_file:
